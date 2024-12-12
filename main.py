0e7d6ee093aacf8857c128ae2a58de620dcb26f8
Hossein Shahverdi <70755266+HosseinShahverdi@users.noreply.github.com>
2024-12-12 16:29:02 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tqdm import tqdm
 from utils.preprocessor import Preprocessor
 from utils.modelResultVisualizer import ModelResultVisualizer
 from utils.logger import get_logger
@@ -23,8 +24,7 @@ if __name__ == "__main__":
         X_train, X_test, y_train, y_test = preprocessor.get_train_test_data()
 
         # Models to evaluate
-        models = {
-            "RandomForest": RandomForestModel(),
+        models = {          
             "LogisticRegression": LogisticRegressionModel(),
             "SGDClassifier": SGDClassifierModel(),
             "SVM": SVMModel(),
@@ -32,18 +32,19 @@ if __name__ == "__main__":
             "AdaBoost": AdaBoostModel(),
             "KNN": KNNModel(),
             "XGBoost": XGBoostModel(),
-            "CatBoost": CatBoostModel()
+            "CatBoost": CatBoostModel(),
+            "RandomForest": RandomForestModel(),
         }
         
         visualizer = ModelResultVisualizer()
 
         # Train and evaluate each model
-        for name, model in models.items():
-            print(f"Training {name}...")
+        for name, model in tqdm(models.items(), desc = "Model Training Progress"):
+            logger.info(f"Training {name}...")
             best_model, best_params = model.train_with_grid_search(X_train, y_train)
             accuracy = best_model.score(X_test, y_test)
-            print(f"Best Parameters for {name}: {best_params}")
-            print(f"{name} Model Accuracy: {accuracy}\n")
+            logger.info(f"Best Parameters for {name}: {best_params}")
+            logger.info(f"{name} Model Accuracy: {accuracy}\n")
             
             
             # Generate predictions and probabilities for visualization
@@ -54,13 +55,16 @@ if __name__ == "__main__":
             visualizer.store_results(name, best_params, accuracy)
 
             # Plot confusion matrix
+            logger.info(f"Plotting confusion matrix for {name}...")
             visualizer.plot_confusion_matrix(y_test, y_pred, class_names=['Class 0', 'Class 1'])
 
             # Plot classification report
+            logger.info(f"Plotting classification report for {name}...")
             visualizer.plot_classification_report(y_test, y_pred)
 
             # Plot ROC curve 
             if y_proba is not None:
+                logger.info(f"Plotting ROC curve for {name}...")
                 visualizer.plot_roc_curve(y_test, y_proba)
                 
     except Exception as e:
